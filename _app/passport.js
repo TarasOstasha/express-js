d458a760e429e0c3b966510b43354a0a537b3c69
TarasOstasha <tonyjoss1990@gmail.com>
2020-03-05 07:13:58 -0500

    build

--- _app/passport.js
+++ _app/passport.js
@@ -33,8 +33,8 @@ var config = {
         callbackURL: "http://127.0.0.1:3600/auth/github/callback"
     },
     google: {
-        clientID: '92005282075-9mhfnmoa0kve9gbhg46vvdpgcsl1ko9j.apps.googleusercontent.com', //process.env.GP_ID, //'706111676047-g5j86f7ipga7ant19ii0shaltrooac36.apps.googleusercontent.com',
-        clientSecret: 'sIkzPyHKsbxkHIU6-xsZTbii', //process.env.GP_KEY, //'IdHthb-IWhRRyGtl1K5dNd38',
+        clientID: '92005282075-90e7p38q48s6vvq6er9hk92k9jh56tcn.apps.googleusercontent.com', //process.env.GP_ID, //'706111676047-g5j86f7ipga7ant19ii0shaltrooac36.apps.googleusercontent.com',
+        clientSecret: 'SATBdfSYF_tMPF65sJIukSxF', //process.env.GP_KEY, //'IdHthb-IWhRRyGtl1K5dNd38',
         // callbackURL: 'http://127.0.0.1:3600/auth/google/callback'
         callbackURL: 'https://tonyjoss.com/auth/google/callback'//process.env.GP_CLB //'http://r4.okm.pub:3600/auth/google/callback'
         // callbackURL: `${process.env.HOST}:3600/auth/google/callback`
