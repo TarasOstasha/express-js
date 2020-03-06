5cfd0e3e15f43c45f91bf0f413bb85df3fdd6b1e
TarasOstasha <tonyjoss1990@gmail.com>
2020-03-05 20:22:46 -0500

    google-remove

--- _app/passport.js
+++ _app/passport.js
@@ -96,7 +96,8 @@ passport.use(new GoogleStrategy({
         // log-s
         log('google profile: ', profile)
         // var-s
-        var email = ''
+        //var email = ''
+        var email = profile.email
         let id = profile.id
         let username = profile.displayName
 
