92e1363fea5fcf4284f8236549156877f0dfa7a4
TarasOstasha <tonyjoss1990@gmail.com>
2020-03-22 20:30:21 -0400

    ...

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use((req, res, done )=>{
   if(req.headers.host == 'localhost' || req.headers.host == 'tonyjoss.com' || req.headers.host == 'tonyjoss.com/main') {
     fs.readFile('portfolio/index.html', 'UTF-8', (err, data)=>{
       if(err) res.send(err);
-      res.setHeader("Content-Type", "text/html");
+      //res.setHeader("Content-Type", "text/html");
       res.send(data) 
     })
   }
